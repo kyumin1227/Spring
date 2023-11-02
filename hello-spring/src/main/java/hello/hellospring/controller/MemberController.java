66594839171e8ce80af2028fb275131800c8279a
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-02 16:55:07 +0900

    Feat: Member 생성 및 조회 페이지 구현 - home.html: 루트 페이지로 멤버 생성, 조회 페이지 링크 - createMemberForm.html: 멤버 생성 페이지 - memberList.html: 멤버 전체 조회 페이지
    - HomeController.java: 루트 페이지의 Get 요청을 받아 home.html 반환
    - MemberController.java: 멤버 생성 페이지의 Get, Post 요청과 멤버 조회 페이지의 Get 요청을 받음
    - MemberForm.java: 멤버 생성 페이지의 Post 요청의 FormData를 자동으로 처리하는 클래스
    (만약 FormData = "name": "kyumin" 일 경우 MemberForm에 name이 있다면 setName을 호출하여 데이터를 설정)

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // Controller 주석을 이용한 함수는 모두 Spring 컨테이너에 자동으로 등록
 public class MemberController {
@@ -14,4 +20,29 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+//    MemberForm으로 값을 받고있기 때문에 MemberForm을 호출하여 formData를 받은 이때
+//    formData로 넘어온 값의 key가 name이기 때문에 Spring이 MemberForm의 name에 setName함수를 활용하여 자동으로 값을 넣어 줌
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);  // html에 넘기는 속성
+        return "members/memberList";
+    }
+
 }
