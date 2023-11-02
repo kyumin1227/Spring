442c58911c93108e3854dee6ec2d8697a563fea6
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-02 15:36:33 +0900

    Test: MemberService 테스트 클래스 생성 - MemberService의 Repository를 생성자를 이용한 생성으로 변경

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+//    생성자로 memberRepository를 받음
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
