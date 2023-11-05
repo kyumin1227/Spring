3fb641ab09bb4119eb329d7c8f6f92ab04ccb89a
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-06 00:01:04 +0900

    Feat: AOP 맛보기

---
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect // AOP는 필수적으로 적어주어야 함
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")    // AOP를 실행시킬 class들의 경로 지정
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+
+    }
+}
