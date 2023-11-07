1644cad64e74acd43b916d171fd1f8fa979a4f39
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-07 15:41:25 +0900

    Feat: article 정보를 h2 DB에 저장 - Article: 엔티티 파일 - ArticleController: dto를 엔티티로 변환 및 저장 - ArticleForm: dto - ArticleRepository: DB에 연결

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
+
 public class ArticleForm {
 
     private String title;
@@ -16,5 +18,9 @@ public class ArticleForm {
     public ArticleForm(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
     }
 }
