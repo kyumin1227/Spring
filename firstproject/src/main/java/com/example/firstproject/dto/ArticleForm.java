e697359f79f6894f90ff0d13def59407e861cb6d
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-07 17:34:32 +0900

    Refactor: 롬복 설치 및 롬복을 이용한 코드 리펙토링

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,28 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor     // 롬복을 이용하여 생성자 코드 대체
+@ToString               // 롬복을 이용하여 toString 코드 대체
 public class ArticleForm {
 
     private String title;
     private String content;
 
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "ArticleForm{" +
+//                "title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
+//    public ArticleForm(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
 
     public Article toEntity() {
         return new Article(null, title, content);
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity     // 엔티티 선언
+@AllArgsConstructor     // 롬복을 이용하여 생성자 대체
+@ToString               // 롬복을 이용하여 toString 대체
 public class Article {
 
     @Id     // 엔티티의 대표값 지정
@@ -16,18 +20,18 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
