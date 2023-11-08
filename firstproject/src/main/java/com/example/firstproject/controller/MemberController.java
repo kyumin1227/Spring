f0f31194e86ae4528ba2453315d3c84754490324
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-08 15:55:54 +0900

    Practice: 회원 id로 조회 및 전체 조회

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -29,5 +33,19 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("members")
+    public String showMembers(Model model) {
+        List<Member> membersEntityList = (List<Member>) memberRepository.findAll();
+        model.addAttribute("membersList", membersEntityList);
+        return "members/index";
+    }
+
+    @GetMapping("members/{id}")
+    public String showMember(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
     }
 }
