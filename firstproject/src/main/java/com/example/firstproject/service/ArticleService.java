d766a850c093c3e5662e021523be8b2086fddb41
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-15 15:24:46 +0900

    Test: Transaction 기능 확인

--- firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -6,8 +6,10 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service    // 서비스 객체 생성
@@ -60,5 +62,21 @@ public class ArticleService {
 //        3. 대상 삭제하기
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional  // org.springframework  중간에 실패할 경우 롤백을 통해 이전 상태로 돌아감
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+//        1. dto 묶음을 엔티티 묶음으로 변환하기
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+//        2. 엔티티 묶음을 DB에 저장하기
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+//        3. 강제 예외 발생시키기
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+//        4. 결과 값 반환하기
+        return articleList;
     }
 }
