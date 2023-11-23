25f918bb3aa8c232acb8be20ba6344818553ecb3
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-23 17:03:03 +0900

    Refactor: 순수 자바 코드 -> 스프링 코드

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(getMemberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 
+    @Bean
     private static MemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     private static DiscountPolicy getDiscountPolicy() {
         return new RateDiscountPolicy();
     }
