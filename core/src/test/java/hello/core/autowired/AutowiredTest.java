31c96014340f957c07f6136b6d9157aba1962f4b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-29 20:03:51 +0900

    Test: Autowired 옵션 테스트 - required에 false를 입력할 경우 값이 없으면 코드를 무시한다.

---
+++ core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,23 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+    }
+}
