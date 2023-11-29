901674d208da583f72af2c698877df9690abfbcf
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-29 22:11:56 +0900

    Fix: Qualifier의 새로운 annotaion을 생성하여 Bean 중복 오류 해결 - 생성자를 lombok으로 생성하여 annotation이 입력되지 않았기 때문에 오류 발생 - 생성자를 직접 작성 후 annotation을 붙여주어 오류 해결

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,12 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor    // 필수 파라미터의 생성자를 생성
+//@RequiredArgsConstructor    // 필수 파라미터의 생성자를 생성
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-    @MainDiscountPolicy
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
