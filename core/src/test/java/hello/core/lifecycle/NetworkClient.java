b068fc0867e5ef591ca123a6d493c4d6441eedd7
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-11-30 19:25:03 +0900

    Test: 빈 생명주기 콜백 테스트 - 2. 빈 메소드를 이용한 생명주기 콜백

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -28,15 +25,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
 //    의존관계 주입이 끝난 후 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
 //    빈이 종료될 때 호출
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         disconnect();
     }
 }
