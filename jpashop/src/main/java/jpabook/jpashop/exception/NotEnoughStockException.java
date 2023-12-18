b4c499f1a80032672ce8ab54837365ec66f18336
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-12-18 20:04:13 +0900

    Feat: 상품 비즈니스 로직, 레포지토리, 서비스 개발 - Item: 비즈니스 로직의 경우에는 Entity에 추가하였음

---
+++ jpashop/src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.exception;
+
+//    재고가 부족할 경우 발생하는 오류
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
