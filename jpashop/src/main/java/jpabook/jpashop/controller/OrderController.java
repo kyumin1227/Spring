cead1002b4f2062d44f5cfee61216978f2a7db10
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-12-26 17:50:46 +0900

    Feat: 상품 주문 내역 조회

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,6 +46,15 @@ public class OrderController {
         orderService.order(memberId, itemId, count);
 
         return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        System.out.println(orders);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
     }
 
 }
